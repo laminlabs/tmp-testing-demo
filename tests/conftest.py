56f3bdc094b849d76e3f91b1763a8f3c253ef147
Lukas Heumos <lukas.heumos@posteo.net>
2025-02-26 18:21:07 +0100

    🎨 CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ import pytest
 
 @pytest.fixture(scope="session")
 def setup_instance():
-    ln_setup.init(storage="./testdb", modules="bionty")
+    ln_setup.init(storage="./testdb", modules="bionty,tmp_testing_demo")
     yield
     ln_setup.delete("testdb", force=True)
