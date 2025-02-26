69114a1e95ec7f25674efa471dcabcc52520b08f
Lukas Heumos <lukas.heumos@posteo.net>
2025-02-26 17:34:26 +0100

    🎉 Initial commit

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,5 @@
 import nox
-from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
+from laminci.nox import run_pre_commit, run_pytest
 
 # we'd like to aggregate coverage information across sessions
 # and for this the code needs to be located in the same
@@ -14,12 +14,9 @@ def lint(session: nox.Session) -> None:
 
 
 @nox.session()
-@nox.parametrize("group", ["unit", "docs"])
+@nox.parametrize("group", ["unit"])
 def build(session, group):
     session.run(*"uv pip install --system -e .[dev]".split())
-    login_testuser1(session)
 
     if group == "unit":
         run_pytest(session)
-    elif group == "docs":
-        build_docs(session, strict=True)
